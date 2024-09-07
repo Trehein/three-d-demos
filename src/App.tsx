cd3f51db6cd75f14a2a236cfb5af314e560ae441
treheinguy <treheinguy@gmail.com>
2024-09-07 10:31:08 -0500

    basic scene

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import FirstScene from './Demos/FirstScene/FirstScene'
+import SimpleCubes from './Demos/SimpleCubes/SimpleCubes'
+
+function App() {
+  return (
+    <>
+      {/* <SimpleCubes /> */}
+      <FirstScene />
+    </>
+  )
+}
+
+export default App
---
+++ src/Demos/FirstScene/FirstScene.tsx
@@ -0,0 +1,35 @@
+import * as THREE from 'three'
+import { Canvas } from '@react-three/fiber'
+import React, { useState } from 'react'
+
+const FirstScene: React.FC = () => {
+  // const cameraPosition = {
+  //   fov: 75, 
+  //   near: 0.1, 
+  //   far: 1000,
+  //   postion: [1000, 50, 5]
+  // }
+
+  const [active, setActive] = useState(false)
+
+  return (
+    <div id='canvas-container'>
+      <Canvas
+        // camera={cameraPosition}
+      >
+        <mesh scale={active ? 1.5 : 1} onClick={() => setActive(!active)}>
+          {/* <boxGeometry args={[2,2,2]} /> */}
+          {/* <octahedronGeometry args={[2,4]} /> */}
+          <sphereGeometry />
+          <meshPhongMaterial color="royalblue" />        
+        </mesh>
+        <ambientLight intensity={0.2} />
+        <directionalLight color="white" position={[0, 50, 25]} />
+      </Canvas>
+    </div>
+  )
+}
+
+75, window.innerWidth / window.innerHeight, 0.1, 1000
+
+export default FirstScene
